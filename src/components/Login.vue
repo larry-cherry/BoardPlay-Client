e244e0d79eab064e1d41fc8438627b00d92f8a7d
Kenny Tang <kennyftang@gmail.com>
2017-07-08 15:57:40 -0500

    Working on login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,55 @@
 <template>
   <div id="login">
-    <h2>Login</h2>
+    <h1>Login</h1>
     <form>
-      <label for="username">Username</label>
-      <input v-model="username" name="username" type="text" />
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input v-model="username" name="username" type="text" />
+      </div>
       </br>
-      <label for="password">Password</label>
-      <input v-model="username" name="password" type="password" />
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input v-model="password" name="password" type="password" />
+      </div>
       </br>
-      <button v-on:click="login()" type="submit">Login</button>
+      <button v-on:click="login()">Login</button>
     </form>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'login',
-  data: {
-    username: '',
-    password: '',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
   },
   methods: {
-    login: () => {
+    login: (e) => {
+      e.preventDefault();
+      console.log('here');
       alert(`Logging in with ${this.username} and ${this.password}`);
     },
   },
 };
 </script>
 
-<style>
+<style scoped>
 div#login form {
   display: inline-flex;
   flex-direction: column;
+}
+div#login h1 {
+  padding-bottom: 2em;
+  font-size: 4em;
+}
+div#login form {
+  font-size: 2em;
+}
+div.form-group label {
+  display: block;
 }
 </style>
