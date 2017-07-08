91b96b8ea5bb8b303cb28e1f1ea62f19bdacd407
Kenny Tang <kennyftang@gmail.com>
2017-07-08 15:15:34 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <login></login>
+  </div>
+</template>
+
+<script>
+// import Hello from './components/Hello';
+import 'bootstrap/dist/css/bootstrap.css';
+import Login from './components/Login';
+
+export default {
+  name: 'app',
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="login">
+    <h2>Login</h2>
+    <form>
+      <label for="username">Username</label>
+      <input v-model="username" name="username" type="text" />
+      </br>
+      <label for="password">Password</label>
+      <input v-model="username" name="password" type="password" />
+      </br>
+      <button v-on:click="login()" type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: {
+    username: '',
+    password: '',
+  },
+  methods: {
+    login: () => {
+      alert(`Logging in with ${this.username} and ${this.password}`);
+    },
+  },
+};
+</script>
+
+<style>
+div#login form {
+  display: inline-flex;
+  flex-direction: column;
+}
+</style>
