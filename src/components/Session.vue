ea99e16f29b9eee92d43e58fff4bee2e33d55864
Kenny Tang <kennyftang@gmail.com>
2017-07-09 09:44:26 -0500

    Fixed a fatal error

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -4,8 +4,25 @@
     {{error}}
   </div>
   <h1>Test {{id}}</h1>
-  <div>
 
+  <div class="players" v-if="stage === 0">
+    <h3>Players</h3>
+    <ul>
+      <li v-for="player in players">
+        {{player.name}}
+      </li>
+    </ul>
+  </div>
+  <div v-else-if="stage === 1">
+    <ul class="list-group">
+      <li v-for="location in locations" class="list-group-item">
+        {{ location }}
+      </li>
+    </ul>
+    <div class="buttons">
+      <button type="button" class="btn btn-default navbar-btn">End Game</button>
+      <button type="button" class="btn btn-default navbar-btn">Leave Game</button>
+    </div>
   </div>
 </div>
 </template>
@@ -35,5 +52,7 @@ export default {
 </script>
 
 <style>
-
+div.buttons {
+  
+}
 </style>
--- src/components/Staging.vue
+++ src/components/Staging.vue
@@ -1,22 +1,5 @@
-  <template>
+<template>
 <div id="lobby" class="container">
-  <div v-if="stage === 0">
-    <h3>Players</h3>
-    <ul>
-      <li v-for="player in players">
-        {{player.name}}
-      </li>
-    </ul>
-  </div>
-  <div v-else-if="stage === 1">
-
-    <ul class="list-group">
-      <li v-for="location in locations" class="list-group-item">
-        {{ location }}
-      </li>
-      </ul>
-      <button type="button" class="btn btn-default navbar-btn">Leave Game</button>
-  </div>
 </div>
 </template>
 
