9e15013781092e473c4f58cbfcca84415fe2a65e
Kenny Tang <kennyftang@gmail.com>
2017-07-08 20:14:47 -0500

    Working on session states

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -1,14 +1,30 @@
 <template>
 <div id="session">
-  <h2>Code: {{$route.param.id}}</h2>
+  <div class="error">
+    {{error}}
+  </div>
+
 </div>
 </template>
 
 <script>
+import { router } from '../main';
+
+const id = this.$route.params.id;
+let error = '';
+let session;
+if (!id) {
+  router.go('lobby');
+}
+fetch(`sessions/${id}`).then((f) => {
+  session = f;
+}, (e) => {
+  error = e;
+});
 export default {
   name: 'Session',
   data() {
-    return {};
+    return { error, session };
   },
   methods: {},
   route: {
